57410341e97b0e9ce7dfedac1d62ffde2aeb4e4e
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-08-16 09:54:39 +0300

    Styled my Play Pause button on the album cards.

--- src/Components/AlbumCard.jsx
+++ src/Components/AlbumCard.jsx
@@ -1,5 +1,6 @@
-import PropTypes from "prop-types"
-import "./AlbumCard.css"
+import PropTypes from "prop-types";
+import "./AlbumCard.css";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function AlbumCard({ albumId, albumImage, albumName, albumArtistName }) {
@@ -7,15 +8,34 @@ function AlbumCard({ albumId, albumImage, albumName, albumArtistName }) {
     albumId: PropTypes.string.isRequired,
     albumImage: PropTypes.string.isRequired,
     albumName: PropTypes.string.isRequired,
-    albumArtistName: PropTypes.string.isRequired
+    albumArtistName: PropTypes.string.isRequired,
   };
   const navigate = useNavigate();
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlayPauseClick = (e) => {
+    e.stopPropagation(); // Prevent triggering the navigation on card click
+    setIsPlaying(!isPlaying);
+    // Handle play/pause functionality here
+  };
+
   const handleClick = () => {
     navigate(`/Albums/${albumId}`);
-  }
+  };
   return (
     <div className="album-card" onClick={handleClick}>
       <img src={albumImage} alt={albumName} className="album-cover" />
+      <div className="play-pause-button" onClick={handlePlayPauseClick}>
+        <img
+          src={
+            isPlaying
+              ? "../../public/pause-button.png"
+              : "../../public/play-button.png"
+          }
+          alt={isPlaying ? "Pause" : "Play"}
+          width="30px"
+        />
+      </div>
       <div className="album-info">
         <p className="album-title">{albumName}</p>
         <p className="artist-name">{albumArtistName}</p>
