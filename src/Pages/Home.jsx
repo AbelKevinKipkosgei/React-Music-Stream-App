b1e5c9a0b208a7800bcfee6c5acff8de95212ada
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-08-17 19:51:45 +0300

    Uploaded some superhero musicians.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,6 @@ function Home() {
   return (
     <div className="home">
       <h2>Check out your favorite albums on the albums page.</h2>
-      <img src="/groovewave/groovewavelarge.jpg" alt="Groovewave album cover" />
     </div>
   );
 }
